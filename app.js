498b00a467265e8412f3922c8d18afccf0ca23ee
pd <pd>
2023-05-28 13:28:55 +0800

    1.0.4

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors=require("cors")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors("http://47.97.22.215"))
 
 
 app.use('/', indexRouter);
